56d7e775fe3fa685930b4b2cf7b062ed6a4b7df2
Alexander Kogan <alexkogan@Alexanders-MacBook-Pro.local>
2024-01-16 15:27:28 +0200

    debugging import - remove all but basic

--- src/export.jsx
+++ src/export.jsx
@@ -1,38 +1,38 @@
-import AdminTable from './components/admin-table';
+// import AdminTable from './components/admin-table';
 import AutoBreadcrumbs from './components/auto-breadcrumbs';
-import AsyncButton from './components/async-button';
-import AvatarSelector from './components/avatar-selector';
-import ConfirmDialog from './components/confirm-dialog';
-import ImageSelector from './components/image-selector';
-import ModalWindow from './components/modal-window';
-import Notifications from './components/notifications';
+// import AsyncButton from './components/async-button';
+// import AvatarSelector from './components/avatar-selector';
+// import ConfirmDialog from './components/confirm-dialog';
+// import ImageSelector from './components/image-selector';
+// import ModalWindow from './components/modal-window';
+// import Notifications from './components/notifications';
 import Spinner from './components/spinner';
-import TinymceEditor from './components/tinymce-editor';
-import Parser, {debounce, pipeline} from './components/parser';
-import {showMessage, deleteMessage, hideMessage, clearAllMessages } from './components/notifications/notificationActions';
-import { actionTypes as notificationsActionTypes} from './components/notifications';
+// import TinymceEditor from './components/tinymce-editor';
+// import Parser, {debounce, pipeline} from './components/parser';
+// import {showMessage, deleteMessage, hideMessage, clearAllMessages } from './components/notifications/notificationActions';
+// import { actionTypes as notificationsActionTypes} from './components/notifications';
 import  notificationsReducers from './components/notifications/reducers';
 
 console.debug("notificationsReducers:", notificationsReducers);
 
 export {
-            AdminTable, 
+            // AdminTable, 
             AutoBreadcrumbs,
-            AsyncButton,
-            AvatarSelector,
-            ConfirmDialog,
-            ImageSelector,
-            ModalWindow,
-            Notifications,
-            Parser,
+            // AsyncButton,
+            // AvatarSelector,
+            // ConfirmDialog,
+            // ImageSelector,
+            // ModalWindow,
+            // Notifications,
+            // Parser,
             Spinner,
-            TinymceEditor,
-            debounce, 
-            pipeline, 
-            showMessage, 
-            hideMessage, 
-            deleteMessage, 
-            clearAllMessages,
-            notificationsActionTypes,
-            notificationsReducers
+            // TinymceEditor,
+            // debounce, 
+            // pipeline, 
+            // showMessage, 
+            // hideMessage, 
+            // deleteMessage, 
+            // clearAllMessages,
+            // notificationsActionTypes,
+            // notificationsReducers
         };
